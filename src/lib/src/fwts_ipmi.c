7fe97b0b1203cb6958d84138d8d4d146b90885d6
Robert.Sz.Cheng <pptsung@gmail.com>
2020-06-11 10:51:34 +0800

    Include poll.h instead of deprecated sys/poll.h

--- src/lib/src/fwts_ipmi.c
+++ src/lib/src/fwts_ipmi.c
@@ -18,10 +18,11 @@
  *
  */
 
+#include <poll.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/poll.h>
 #include <sys/ioctl.h>
 #include <linux/ipmi.h>
 
--- src/lib/src/fwts_pipeio.c
+++ src/lib/src/fwts_pipeio.c
@@ -30,9 +30,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <paths.h>
+#include <poll.h>
 
 #include <sys/param.h>
-#include <sys/poll.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
